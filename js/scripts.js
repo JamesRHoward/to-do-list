a0c49ea04bc4cf7a6acdeadf0fc9e721b81195e6
James Howard and Matthew Reyes <james@>
2016-06-13 13:20:41 -0700

    created editer

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 
     var newToDoItem = new toDoItem(inputtedItem, inputtedDetail);
 
-    $("#list").append("<li class='li-to-do'><span class='list-item'>" + newToDoItem.itemToDo + "</span><button type='remove' class='btn btn-remove'>Done?</button></li>");
+    $("#list").append("<li class='li-to-do'><span class='list-item'>" + newToDoItem.itemToDo + "</span><input type='text' class='edit'><button type='remove' class='button-remove'><img src='img/small-red-x.png'></button><button type='edit' class='button-edit'><img src='img/edit-pencil.png'></button></li>");
 
     $(".list-item").last().click(function(){
       $("#show-to-do").show();
@@ -27,10 +27,18 @@ $(document).ready(function(){
 
 
    });
-   $(".btn-remove").click(function(){
+   $(".button-remove").click(function(){
      $(this).parent().remove();
      $("#show-to-do").hide();
    });
+
+   $(".button-edit").click(function(){
+     $(this).siblings(".list-item").hide().siblings(".edit").show().val($(this).text()).focus();;
+   });
+
+   $(".edit").focusout(function(){
+    $(this).hide().siblings(".list-item").show().text($(this).val());
+});
 
 
 
